52edf99a1e3f87c5d22362d6ffc345bf54f1fb33
sazidl <sazidl@umich.edu>
2025-10-02 17:39:56 -0400

    jenkins

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
-            System.out.println("On new branch" + i);
+            System.out.println("On new branch. Working on Jenkins now" + i);
         }
     }
 }
